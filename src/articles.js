050ee376201280c1ca3e8cf10c442132b5d63461
Rita Petillo <rita.petillo@me.com>
2021-10-01 03:54:41 +0200

    my commit

---
+++ src/articles.js
@@ -0,0 +1,12 @@
+
+
+
+export async function handler(event, context){
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'text/html',
+        },
+        body: '<h1>Hello World</h1>',
+    };
+}
